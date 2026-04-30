45ef084e77540463e088e3580b8c5bc4e8b1ef96
Ezz1997 <ezmarie97@gmail.com>
2026-04-30 21:17:14 +0300

    Add main game objects

---
+++ script.js
@@ -0,0 +1,16 @@
+const GAME_RESULT = Object.freeze({
+    PLAYER1_WINS: "PLAYER1_WINS",
+    PLAYER2_WINS: "PLAYER2_WINS",
+    DRAW: "DRAW"
+});
+
+const TURN = Object.freeze({
+    PLAYER1: "PLAYER1",
+    PLAYER2: "PLAYER2"
+});
+
+const GAME_STATE = Object.freeze({
+    IDLE: "IDLE",
+    ONGOING: "ONGOING",
+    OVER: "OVER"
+});
